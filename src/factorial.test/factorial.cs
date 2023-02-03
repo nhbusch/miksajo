c0fbc8a265f9e26e205a998753bb47d08cb45cd5
Nils Busch <nh.busch@web.de>
2023-02-03 12:17:27 +0100

    Initial commit

---
+++ src/factorial.test/factorial.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace math.numerics.test.specialfunctions
+{
+    public class Factorial
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
