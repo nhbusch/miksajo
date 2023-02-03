683b3b63910e911bed2d36e3b62df3fb09c3ab86
Nils Busch <nh.busch@web.de>
2023-02-03 12:17:27 +0100

    Correct inline documentation

--- src/factorial/factorial.cs
+++ src/factorial/factorial.cs
@@ -68,7 +68,7 @@ namespace math.numerics
         /// Computes the factorial function x -> x! of an integer number > 0. The function can represent all number up
         /// to 22! exactly, all numbers up to 170! using a double representation. All larger values will overflow.
         /// </summary>
-        /// <returns>A value value! for value > 0</returns>
+        /// <returns>A scalar value x! for x > 0</returns>
         /// <exception cref="ArgumentOutOfRangeException" />
         public static double Factorial(int x)
         {
